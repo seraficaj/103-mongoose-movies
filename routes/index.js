85bdd324255c31c6fd4d6854497f8f087d4eee74
Jason Serafica <seraficaj@gmail.com>
2022-11-03 15:03:18 -0700

    Step 9 - Define Routes for Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
-router.get('/', function(req, res, next) {
-  res.redirect('/movies');
+router.get("/", function (req, res, next) {
+  res.redirect("/movies");
+});
+
+// Google OAuth login route
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
+);
+
+// Google OAuth callback route
+router.get(
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/movies",
+    failureRedirect: "/movies",
+  })
+);
+
+// Oauth Logout Route
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/movies");
 });
 
 module.exports = router;
